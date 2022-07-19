d8423c850f5bc23e011f64803be94accc910748c
Andrei Shkurko <andrey.shkurko@mbicycle.com>
2022-07-19 12:55:08 +0300

    init project

---
+++ src/main/java/com/mbicyble/chatting_room/ChattingRoomApplication.java
@@ -0,0 +1,13 @@
+package com.mbicyble.chatting_room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattingRoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mbicyble/chatting_room/ChattingRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbicyble.chatting_room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
